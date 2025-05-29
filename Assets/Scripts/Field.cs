ee9e066108d23617e17aa671e63773bf2fb55fbe
mwhiTwo <unkownperson002@gmail.com>
2025-05-29 19:48:36 +0800

    Updating of Electric Field during runtime is fixed.

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -2,13 +2,28 @@ using UnityEngine;
 
 public class Field : MonoBehaviour
 {
-    public Particle particle; // Assign this in the Inspector
-    
+    public Particle particle;
+
+    private float previousCharge; //to check the previous charge
+
     void Start()
     {
+        previousCharge = particle.charge;
+        UpdateFieldScale();
+    }
+
+    void Update()
+    {
+        if (particle.charge != previousCharge)
         {
-            float absCharge = Mathf.Abs(particle.charge);
-            transform.localScale += new Vector3 (absCharge, absCharge, absCharge);
+            UpdateFieldScale();
+            previousCharge = particle.charge;
         }
+    }
+
+    void UpdateFieldScale()
+    {
+        float electricField = Mathf.Abs(particle.charge);
+        transform.localScale = Vector3.one + new Vector3(electricField, electricField, electricField);
     }
 }
